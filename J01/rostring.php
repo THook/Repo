dbda9cf4ad1fd2d18bb4e65e8e7dabaa43d350cf
Hugo VILLAIN <hvillain@e1r8p10.42.fr>
2014-04-01 22:28:45 +0200

    j01

--- J01/rostring.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-	if ($argc > 1) {
-		$new_str = preg_split('/[\s ]+/', $argv[1], -1, PREG_SPLIT_NO_EMPTY);
-		$new_str[] = $new_str[0];
-		unset($new_str[0]);
-		echo implode(' ', $new_str)."\n";
-	}
-?>
