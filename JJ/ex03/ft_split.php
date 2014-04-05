28c41c3955feb5ccd21d75785d5a399ab2674985
Hugo VILLAIN <hvillain@e1r12p9.42.fr>
2014-04-05 04:58:33 +0200

    a

--- JJ/ex03/ft_split.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php
-<?php
-	function ft_split($string){
-		$array = array_filter(explode(' ', $string));
-		foreach ($array as $key => $value)
-			$ret[] = $value;
-		return ($ret);
-	}
-?>
--- JJ/ex08/ft_is_sort.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function ft_is_sort($tab){
-		$tab_sort = $tab;
-		sort($tab_sort);
-		if ($tab_sort == $tab)
-			return true;
-		return false;
-	}
-?>
