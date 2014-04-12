ab28a1c0f053eaaad0cee12d71d91fd998e4079d
Hugo VILLAIN <hvillain@e1r12p10.42.fr>
2014-04-12 07:49:29 +0200

    rush deleted

--- rush/htdocs/functions/register_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-function register_user($infos){
-$handle = fopen("users/users", 'a');
-$crypt = hash('whirlpool', $infos[password]);
-fwrite($handle, $infos[login].";".$crypt."read\n");
-fclose($handle);
-}
-?>
